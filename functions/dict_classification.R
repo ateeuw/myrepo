0c37e2078846687e788bfdc81c00b8f17da8d541
Teeuwen <a.s.teeuwen@utwente.nl>
2021-04-01 14:50:32 +0200

    had some problems so made sure all data were in the right format. In the end that was not the problem, but it doesn't hurt so kept it in. Also cleaned it a bit

--- functions/dict_classification.R
+++ functions/dict_classification.R
@@ -1,15 +1,15 @@
 #This function goes through a given dictionary and add the class a code belongs to in a new column
 
 dict_classification <- function(sheet, dct, clm, class_clm){
+  sheet <- as.data.frame(sheet)
   sheet[, class_clm] <- as.character(sheet[, class_clm])
-  #sheet[i, n_clm] <- as.numeric(sheet[i, n_clm])
+  sheet[, clm] <- as.character(sheet[, clm])
   for(i in 1:nrow(sheet)){
     print(i)
     for(j in names(dct)){
       print(j)
       if((sheet[i,clm] %in% dct[[j]])){
         sheet[i, class_clm] <- j
-        #sheet[i, n_clm] <- length(dct[[j]])
       }
     }
   }
