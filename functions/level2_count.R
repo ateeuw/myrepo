f4717e97f2557afd6a7bdba78678d6e914e21cd8
Teeuwen <a.s.teeuwen@utwente.nl>
2021-04-01 14:49:28 +0200

    I don't think I will use this one anymore, made some changes, but the level2_summ is better

--- functions/level2_count.R
+++ functions/level2_count.R
@@ -2,14 +2,14 @@
 
 level2_count <- function(sheet){
   data_sum <- sheet %>% 
-    group_by(name, ID) %>%
-    count(name)
+    group_by(Document, name, class) %>%
+    count(Document, name, class)
   
   data_sum$n <- 1
   
   data_sum <- data_sum %>% 
-    group_by(name) %>%
-    count(name)
+    group_by(class) %>%
+    count(class)
   
   data_sum <- data_sum %>%
     arrange(desc(n))
